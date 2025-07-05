cdd91aec48d919893c2b45ca38bd08a919c122de
Goodkidya <58204084+Goodkidya@users.noreply.github.com>
2025-07-05 19:21:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -208,8 +208,9 @@ def edit_book(book_id):
         return render_template("edit_book.html", book=book)
 
 
-if __name__ == '__main__':
-    app.run(port=5000,debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
 
