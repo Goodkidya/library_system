9984d7ea60492c88df29e0c2e70323e0f6a6f2e4
Goodkidya <58204084+Goodkidya@users.noreply.github.com>
2025-07-15 15:09:12 +0800

    version 9.0

--- app.py
+++ app.py
@@ -90,7 +90,9 @@ def add_book():
     author = request.form['author']
     conn = get_conn()
     cur = conn.cursor()
-    cur.execute("INSERT INTO books (title, author) VALUES (%s, %s)", (title, author))
+    # available default true
+    # cur.execute("INSERT INTO books (title, author) VALUES (%s, %s)", (title, author))
+    cur.execute("INSERT INTO books (title, author, available) VALUES (%s, %s, TRUE)", (title, author))
     conn.commit()
     cur.close()
     conn.close()
@@ -129,6 +131,7 @@ def borrow():
     return render_template('borrow.html', readers=readers, books=books)
 
 # --- Return ---
+borrow_date = datetime.now()
 @app.route('/return', methods=['GET', 'POST'])
 def return_book():
     conn = get_conn()
@@ -156,6 +159,7 @@ def return_book():
     return render_template('return.html', records=records)
 
 # --- Records ---
+borrow_date = datetime.now()
 @app.route('/records')
 def records():
     conn = get_conn()
